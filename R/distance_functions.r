2718574462b5f0d80d3ec9d65a744ea4b8dcf8dc
Nello Blaser <nello.blaser@uib.no>
2017-02-24 12:01:21 +0100

    added wrappers

---
+++ R/distance_functions.r
@@ -0,0 +1,30 @@
+#' Pairwise distances
+#' 
+#' Computes distance between each pair of the two collections of inputs.
+#' 
+#' @param X,Y A matrix
+#' @param metric The distance metric to use
+cdist <- function(X, Y, metric = "euclidean"){
+  # make sure input is well-defined
+  metric <- match.arg(metric)
+  X <- as.matrix(X)
+  Y <- as.matrix(Y)
+  # use metric
+  switch(metric, 
+         "euclidean" = euclidean_distance(X, Y))
+}
+
+#' Pairwise distances
+#' 
+#' Computes pairwise distance between observations.
+#' 
+#' @param X A matrix
+#' @param metric The distance metric to use
+pdist <- function(X, metric = "euclidean"){
+  # make sure input is well-defined
+  metric <- match.arg(metric)
+  X <- as.matrix(X)
+  # use metric
+  switch(metric, 
+         "euclidean" = pairwise_euclidean_distance(X))
+}
--- tests/testthat/test-euclidean.R
+++ tests/testthat/test-euclidean.R
@@ -2,5 +2,5 @@ context("euclidean")
 
 test_that("euclidean metric works as expected", {
   x <- as.matrix(runif(100))
-  expect_equivalent(as.matrix(dist(x)), pairwise_euclidean_distance(x))
+  expect_equivalent(as.matrix(dist(x)), pdist(x))
 })
