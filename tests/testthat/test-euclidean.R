03cf547e9d9fde9c5671376cd7ce166e637a17a8
Nello Blaser <nello.blaser@uib.no>
2017-02-24 13:04:23 +0100

    added some tests for cdist

--- tests/testthat/test-euclidean.R
+++ tests/testthat/test-euclidean.R
@@ -2,5 +2,10 @@ context("euclidean")
 
 test_that("euclidean metric works as expected", {
   x <- as.matrix(runif(100))
-  expect_equivalent(as.matrix(dist(x)), pdist(x))
+  dist_mat <- as.matrix(dist(x))
+  # check pdist and cdist 
+  expect_equivalent(dist_mat, pdist(x))
+  expect_equivalent(dist_mat, cdist(x, x))
+  expect_equivalent(dist_mat[1:2, 3:100], cdist(x[1:2, ], x[3:100, ]))
+  expect_equivalent(dist_mat[1, 2:100, drop = FALSE], cdist(x[1, ], x[2:100, ]))
 })
