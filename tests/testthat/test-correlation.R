ccfaa464b708df3600e02002b522c49af36f10b0
Nello Blaser <blasernello@hotmail.com>
2017-02-26 00:04:05 +0100

    added tolerance to tests

--- tests/testthat/test-correlation.R
+++ tests/testthat/test-correlation.R
@@ -13,11 +13,11 @@ test_that("correlation metric works as expected", {
   dist_mat <- sqrt((1 - bound(cor(t(x))))/2)
   dist_dist <- as.dist(dist_mat)
   # check pdist and cdist 
-  expect_equivalent(dist_dist, rdist(x, metric = "correlation"))
-  expect_equal(dist_mat, pdist(x, metric = "correlation"))
-  expect_equal(dist_mat, cdist(x, x, metric = "correlation"))
-  expect_equal(dist_mat[1:2, 3:100], cdist(x[1:2, ], x[3:100, ], metric = "correlation"))
-  expect_equal(dist_mat[1, 2:100, drop = FALSE], cdist(x[1, , drop = FALSE], x[2:100, ], metric = "correlation"))
+  expect_equal(dist_dist, rdist(x, metric = "correlation"), tolerance = 1e-7, check.attributes = FALSE)
+  expect_equal(dist_mat, pdist(x, metric = "correlation"), tolerance = 1e-7)
+  expect_equal(dist_mat, cdist(x, x, metric = "correlation"), tolerance = 1e-7)
+  expect_equal(dist_mat[1:2, 3:100], cdist(x[1:2, ], x[3:100, ], metric = "correlation"), tolerance = 1e-7)
+  expect_equal(dist_mat[1, 2:100, drop = FALSE], cdist(x[1, , drop = FALSE], x[2:100, ], metric = "correlation"), tolerance = 1e-7)
 })
 
 test_that("angular metric works as expected", {
@@ -27,8 +27,8 @@ test_that("angular metric works as expected", {
   dist_mat <- acos(bound(cor(t(x))))
   dist_dist <- as.dist(dist_mat)
   # check pdist and cdist 
-  expect_equivalent(dist_dist, rdist(x, metric = "angular"))
-  expect_equal(dist_mat, pdist(x, metric = "angular"))
+  expect_equal(dist_dist, rdist(x, metric = "angular"), tolerance = 1e-7, check.attributes = FALSE)
+  expect_equal(dist_mat, pdist(x, metric = "angular"), tolerance = 1e-7)
   expect_equal(dist_mat, cdist(x, x, metric = "angular"), tolerance = 1e-7)
   expect_equal(dist_mat[1:2, 3:100], cdist(x[1:2, ], x[3:100, ], metric = "angular"), tolerance = 1e-7)
   expect_equal(dist_mat[1, 2:100, drop = FALSE], cdist(x[1, , drop = FALSE], x[2:100, ], metric = "angular"), tolerance = 1e-7)
@@ -41,8 +41,8 @@ test_that("absolute correlation metric works as expected", {
   dist_mat <- sqrt((1 - bound(cor(t(x)))^2))
   dist_dist <- as.dist(dist_mat)
   # check pdist and cdist 
-  expect_equivalent(dist_dist, rdist(x, metric = "absolute_correlation"))
-  expect_equal(dist_mat, pdist(x, metric = "absolute_correlation"))
+  expect_equal(dist_dist, rdist(x, metric = "absolute_correlation"), tolerance = 1e-7, check.attributes = FALSE)
+  expect_equal(dist_mat, pdist(x, metric = "absolute_correlation"), tolerance = 1e-7)
   expect_equal(dist_mat, cdist(x, x, metric = "absolute_correlation"), tolerance = 1e-7)
   expect_equal(dist_mat[1:2, 3:100], cdist(x[1:2, ], x[3:100, ], metric = "absolute_correlation"), tolerance = 1e-7)
   expect_equal(dist_mat[1, 2:100, drop = FALSE], cdist(x[1, , drop = FALSE], x[2:100, ], metric = "absolute_correlation"), tolerance = 1e-7)
