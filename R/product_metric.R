31aa1d6d38a67085c55769ce6f6fd8221b578dda
Nello Blaser <nello.blaser@uib.no>
2017-08-24 15:50:07 +0200

    added product metric

---
+++ R/product_metric.R
@@ -0,0 +1,38 @@
+#' Product metric
+#' 
+#' Returns the p-product metric of two metric spaces. 
+#' Works for output of `rdist`, `pdist` or `cdist`.
+#' 
+#' @param ... Distance matrices or dist objects
+#' @param p The power of the Minkowski distance
+#' 
+#' @examples 
+#' # generate data
+#' df <- matrix(runif(200), ncol = 2)
+#' # distance matrices
+#' dist_mat <- pdist(df)
+#' dist_1 <- pdist(df[, 1])
+#' dist_2 <- pdist(df[, 2])
+#' # product distance matrix
+#' dist_prod <- product_metric(dist_1, dist_2)
+#' # check equality
+#' all.equal(dist_mat, dist_prod)
+#' @export
+product_metric <- function(..., p = 2){
+  metrics <- list(...)
+  # check for equal dimension
+  stopifnot(length(unique(lapply(metrics, dim))) == 1, 
+            length(unique(lapply(metrics, length))) == 1)
+  if (p < Inf){
+    p_metrics <- lapply(metrics, `^`, p)
+    p_result <- Reduce(`+`, p_metrics)
+    res <- p_result ^ (1/p)
+  }
+  else {
+    res <- pmax(...)
+  }
+  if (is(metrics[[1]], "dist")) {
+    attr(res, "call") <- match.call()
+  } 
+  return(res)
+}
